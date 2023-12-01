83b66523f8fdd6f5cdb8e52841ee8fabe8062bb0
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-12-01 21:19:49 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,59 @@
-// Choose a cache name with versioning
-const cacheName = 'cache-v7';
+// service-worker.js
 
-// List the files to precache
-const precacheResources = ['/', '/index.html', '/matiere.html', '/st.html', '/info.html', '/learn', '/learn/index.html', '/style.css', '/js/main.js', '/js/script.js', '/js/modal.js', '/learn/library.html', '/img/texture.png',  '/img/math/MAT-ARI-0001.png', '/img/faviconcolor-fixed.png', '/img/math/MAT-ARI-0011.png', '/img/0.png', '/img/1.png', '/api/math.json', '/api/lv1.json', '/api/lv2.json'];
+const CACHE_NAME = 'my-cache';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/matiere.html',
+  '/st.html',
+  '/info.html',
+  '/learn',
+  '/learn/index.html',
+  '/style.css',
+  '/js/main.js',
+  '/js/script.js',
+  '/js/modal.js',
+  '/learn/library.html',
+  '/img/texture.png',
+  '/img/math/MAT-ARI-0001.png',
+  '/img/faviconcolor-fixed.png',
+  '/img/math/MAT-ARI-0011.png',
+  '/img/0.png',
+  '/img/1.png',
+  '/api/math.json',
+  '/api/lv1.json',
+  '/api/lv2.json'
+];
 
-// When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
-  console.log('Service worker install event!');
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
-});
-
-// When the service worker is activating, delete old caches and take control immediately
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activate event!');
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== cacheName) {
-            return caches.delete(cache);
-          }
-        })
-      );
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
     })
   );
-  return self.clients.claim(); // Take control immediately
 });
 
-// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
 self.addEventListener('fetch', (event) => {
-  console.log('Fetch intercepted for:', event.request.url);
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request);
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        // If the request is in the cache and the device is offline, return the cached response
+        if (cachedResponse && !navigator.onLine) {
+          return cachedResponse;
+        }
+
+        // If the device is online, fetch the resource from the network
+        return fetch(event.request).then((networkResponse) => {
+          // Update the cache with the new response
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        }).catch(() => {
+          // If fetching from the network fails, return the cached response (if available)
+          if (cachedResponse) {
+            return cachedResponse;
+          }
+        });
+      });
     })
   );
 });
