9c78e8b0282e5432887371806df570a675386e21
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-12-01 20:48:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Choose a cache name with versioning
-const cacheName = 'cache-v6';
+const cacheName = 'cache-v7';
 
 // List the files to precache
 const precacheResources = ['/', '/index.html', '/matiere.html', '/st.html', '/info.html', '/learn', '/learn/index.html', '/style.css', '/js/main.js', '/js/script.js', '/js/modal.js', '/learn/library.html', '/img/texture.png',  '/img/math/MAT-ARI-0001.png', '/img/faviconcolor-fixed.png', '/img/math/MAT-ARI-0011.png', '/img/0.png', '/img/1.png', '/api/math.json', '/api/lv1.json', '/api/lv2.json'];
