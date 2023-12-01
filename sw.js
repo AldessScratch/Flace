c19b00e37acd83f73ba572957d4ca547428dc5ac
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-12-01 12:17:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,22 @@
-// Service Worker pour que flace marche hors connexion
+// Choose a cache name with versioning
+const cacheName = 'cache-v4';
 
-
-// Choose a cache name
-const cacheName = 'cache-v3';
 // List the files to precache
-const precacheResources = ['/', '/index.html', '/matiere.html', '/st.html', '/learn', '/learn/index.html', '/style.css', '/js/main.js', '/js/script.js', '/js/modal.js', '/learn/library.html', '/img/texture.png',  '/img/math/MAT-ARI-0001.png', '/img/faviconcolor-fixed.png', '/img/math/MAT-ARI-0011.png', '/img/0.png', '/img/1.png', '/api/math.json', '/api/lv2.json'];
+const precacheResources = ['/', '/index.html', '/matiere.html', '/st.html', '/learn', '/learn/index.html', '/style.css', '/js/main.js', '/js/script.js', '/js/modal.js', '/learn/library.html', '/img/texture.png',  '/img/math/MAT-ARI-0001.png', '/img/faviconcolor-fixed.png', '/img/math/MAT-ARI-0011.png', '/img/0.png', '/img/1.png', '/api/math.json', '/api/lv1.json', '/api/lv2.json'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => {
-        return cache.addAll(cacheName);
-      })
-      .then(() => {
-        return self.skipWaiting();
-      })
-  );
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
 });
 
-self.addEventListener('activate', event => {
+// When the service worker is activating, delete old caches and take control immediately
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cache => {
+        cacheNames.map((cache) => {
           if (cache !== cacheName) {
             return caches.delete(cache);
           }
@@ -31,8 +24,8 @@ self.addEventListener('activate', event => {
       );
     })
   );
+  return self.clients.claim(); // Take control immediately
 });
-
 
 // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
 self.addEventListener('fetch', (event) => {
@@ -43,6 +36,6 @@ self.addEventListener('fetch', (event) => {
         return cachedResponse;
       }
       return fetch(event.request);
-    }),
+    })
   );
 });
