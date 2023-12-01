8d217e73dc9a3c74993235a31d056406a6398193
Aldess <106018979+AldessScratch@users.noreply.github.com>
2023-12-01 12:11:40 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,19 +2,37 @@
 
 
 // Choose a cache name
-const cacheName = 'cache-v2';
+const cacheName = 'cache-v3';
 // List the files to precache
 const precacheResources = ['/', '/index.html', '/matiere.html', '/st.html', '/learn', '/learn/index.html', '/style.css', '/js/main.js', '/js/script.js', '/js/modal.js', '/learn/library.html', '/img/texture.png',  '/img/math/MAT-ARI-0001.png', '/img/faviconcolor-fixed.png', '/img/math/MAT-ARI-0011.png', '/img/0.png', '/img/1.png', '/api/math.json', '/api/lv2.json'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
-self.addEventListener('install', (event) => {
-  console.log('Service worker install event!');
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(cacheName);
+      })
+      .then(() => {
+        return self.skipWaiting();
+      })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Service worker activate event!');
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== cacheName) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
+
 
 // When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
 self.addEventListener('fetch', (event) => {
