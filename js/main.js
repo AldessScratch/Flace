892362ccfc24e6888f8fa43dda8a00c8afab9b83
Pix4rt-XIX <113467410+Pix4rt-XIX@users.noreply.github.com>
2023-12-08 21:34:07 +0100

    added FIREBASE on main.js and modules

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+// FIREBASE
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8i9HXiWElS_KR5wvaAcZ6Je798tq7S_8",
+  authDomain: "flacecards.firebaseapp.com",
+  projectId: "flacecards",
+  storageBucket: "flacecards.appspot.com",
+  messagingSenderId: "796361712607",
+  appId: "1:796361712607:web:4a20ac6f4b19e93b915a6a",
+  measurementId: "G-EEYSL6CXVF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 // Service Worker pour que flace marche hors connexion
 if (navigator.serviceWorker){
  navigator.serviceWorker.register('../sw.js');
